dd51395f3c5682c7df75d1e94e22b41a491f98c1
Tong <header@163.com>
2016-05-29 20:29:51 +0800

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 /**
  * @introduction  Entry
- * @authors       Tongao (email:tongao@yhd.cn)
- * @date          2016-04-26 10:18
- * @update        2016-04-26 10:18
+ * @authors       Null (email:Null@yhd.cn)
+ * @date          Null
+ * @update        Null
  */
 
 import '../css/app.scss';
